f96910b1fc73a786b221247fb0f9b49d5e9ff15b
kazurayam <kazuaki.urayama@gmail.com>
2021-02-08 17:26:26 +0900

    update test_duckduckgo_pageobject.py

--- pywebui/tests/test_duckduckgo_pageobject.py
+++ pywebui/tests/test_duckduckgo_pageobject.py
@@ -25,18 +25,31 @@ def config():
     return data
 
 
+@pytest.fixture(scope='session')
+def config_browser(config):
+    if 'browser' not in config:
+        raise Exception('The config file does not contain "browser"')
+    elif config['browser'] not in ['chrome', 'firefox']:
+        raise Exception(f'"{config["browser"]}" is not a supported browser')
+    return config['browser']
+
+
+@pytest.fixture(scope='session')
+def config_wait_time(config):
+    return config['wait_time'] if 'wait_time' in config else 10
+
+
 @pytest.fixture
-def browser(config):
-    if config['browser'] == 'chrome':
-        # Initialize ChromeDriver
+def browser(config_browser, config_wait_time):
+    if config_browser == 'chrome':
         driver = Chrome()
-    elif config['browser'] == 'firefox':
+    elif config_browser == 'firefox':
         driver = Firefox()
     else:
-        raise Exception(f'"{config["browser"]}" is not a supported browser')
+        raise Exception(f'"{config_browser}" is not a supported browser')
 
     # Wait implicitly for elements to be ready before attempting interactions
-    driver.implicitly_wait(config['wait_time'])
+    driver.implicitly_wait(config_wait_time)
 
     # Return the driver object at the end of setup
     yield driver
