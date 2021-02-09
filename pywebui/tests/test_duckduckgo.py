aef198431b6c2c7d16c8388df1155ab50de49e26
kazurayam <kazuaki.urayama@gmail.com>
2021-02-09 12:41:09 +0900

    add pywebui/tests/conftest.py

--- pywebui/tests/test_duckduckgo.py
+++ pywebui/tests/test_duckduckgo.py
@@ -22,45 +22,6 @@ from duckduckgopages.search import DuckDuckGoSearchPage
 from duckduckgopages.result import DuckDuckGoResultPage
 
 
-@pytest.fixture(scope='session')
-def config():
-    with open('tests/config.json') as config_file:
-        data = json.load(config_file)
-    return data
-
-
-@pytest.fixture(scope='session')
-def config_browser(config):
-    if 'browser' not in config:
-        raise Exception('The config file does not contain "browser"')
-    elif config['browser'] not in ['chrome', 'firefox']:
-        raise Exception(f'"{config["browser"]}" is not a supported browser')
-    return config['browser']
-
-
-@pytest.fixture(scope='session')
-def config_wait_time(config):
-    return config['wait_time'] if 'wait_time' in config else 10
-
-
-@pytest.fixture
-def browser(config_browser, config_wait_time):
-    if config_browser == 'chrome':
-        driver = Chrome()
-    elif config_browser == 'firefox':
-        driver = Firefox()
-    else:
-        raise Exception(f'"{config_browser}" is not a supported browser')
-
-    # Wait implicitly for elements to be ready before attempting interactions
-    driver.implicitly_wait(config_wait_time)
-
-    # Return the driver object at the end of setup
-    yield driver
-
-    # For clean up, quit the driver
-    driver.quit()
-
 # @pytest.mark.skip(reason="DuckDuckGoのテストをわざとスキップする")
 def test_basic_duckduckgo_search(browser):
     PHRASE = 'panda'
---
+++ pywebui/tests/test_flaskr.py
@@ -0,0 +1,16 @@
+"""
+Automated Web UI testing for the flaskr-kazurayam application
+developed by the pywebapp subproject.
+
+0. We assume the Docker container of pywebapp is running.
+1. Navigate to http://localhost:80/
+2. Navigate to the Register form page where
+    we register a new credential.
+    We dynamically generate Username/Password
+    based on the current timestamp.
+3. Do Login using the newly added credential.
+4. Make a new post.
+5. Verify if the post is visible in the list.
+6. Edit the post text and save it.
+7. Verify if the post text is updated in the list.
+"""
