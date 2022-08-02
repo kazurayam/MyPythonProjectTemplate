79052095de3de72d9bafbc912d489501b3524643
kazurayam <kazuaki.urayama@gmail.com>
2022-08-02 16:42:15 +0900

    #8 update test_money.py

--- pycliapp/tests/test_money.py
+++ pycliapp/tests/test_money.py
@@ -1,21 +1,29 @@
 # https://docs.pytest.org/en/latest/
+import pytest
+
 from mypkg.money import Money
 
 
-def test_amount():
-    m = Money(12, "CHF")
-    assert m.amount() == 12
+class TestMoney:
 
+    @pytest.fixture
+    def f12chf(self):
+        return Money(12, "CHF")
 
-def test_currency():
-    m = Money(12, "CHF")
-    assert m.currency() == "CHF"
+    @pytest.fixture
+    def f14chf(self):
+        return Money(14, "CHF")
 
+    def test_amount(self, f12chf):
+        m = f12chf
+        assert m.amount() == 12
 
-def test_add():
-    f12chf = Money(12, "CHF")
-    f14chf = Money(14, "CHF")
-    result = f12chf.add(f14chf)
-    assert result.amount() == 26
-    assert result.currency() == "CHF"
+    def test_currency(self, f14chf):
+        m = f14chf
+        assert m.currency() == "CHF"
+
+    def test_add(self, f12chf, f14chf):
+        result = f12chf.add(f14chf)
+        assert result.amount() == 26
+        assert result.currency() == "CHF"
 
